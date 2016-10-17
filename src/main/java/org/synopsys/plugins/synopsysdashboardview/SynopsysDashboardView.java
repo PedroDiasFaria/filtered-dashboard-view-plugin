d9e8bb6afc8017cc194ac93ae324f9f8c285e46a
Pedro Faria <faria@internal.synopsys.com>
2016-10-17 17:26:13 +0100

    initial commit

---
+++ src/main/java/org/synopsys/plugins/synopsysdashboardview/SynopsysDashboardView.java
@@ -0,0 +1,341 @@
+package org.synopsys.plugins.synopsysdashboardview;
+
+import hudson.Extension;
+import hudson.model.*;
+import hudson.security.Permission;
+import hudson.util.RunList;
+import jenkins.model.Jenkins;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+import org.kohsuke.stapler.export.Exported;
+import org.kohsuke.stapler.export.ExportedBean;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+/**
+ * Created by faria on 10-Oct-16.
+ */
+public class SynopsysDashboardView extends View{
+
+
+    //From missioncontrol
+    private transient int getBuildsLimit;
+    private String viewName;
+    private int fontSize;
+    private int buildQueueSize;
+    private int buildHistorySize;
+    private boolean useCondensedTables;
+    private String statusButtonSize;
+    private String layoutHeightRatio;
+    private String filterRegex;
+
+
+    @DataBoundConstructor
+    public SynopsysDashboardView(String name, String viewName) {
+        super(name);
+
+        //From missioncontrol
+        /**************/
+        this.viewName = viewName;
+        this.fontSize = 16;
+        this.buildQueueSize = 10;
+        this.buildHistorySize = 16;
+        this.useCondensedTables = false;
+        this.statusButtonSize = "";
+        this.layoutHeightRatio = "6040";
+        this.filterRegex = null;
+        /**************/
+    }
+
+    @Override
+    public Collection<TopLevelItem> getItems() {
+        return new ArrayList<TopLevelItem>();
+    }
+
+    @Override
+    protected void submit(StaplerRequest req) throws ServletException, IOException {
+        JSONObject json = req.getSubmittedForm();
+
+        //From missioncontrol
+        /**********************/
+        this.fontSize = json.getInt("fontSize");
+        this.buildHistorySize = json.getInt("buildHistorySize");
+        this.buildQueueSize = json.getInt("buildQueueSize");
+        this.useCondensedTables = json.getBoolean("useCondensedTables");
+        if (json.get("useRegexFilter") != null ) {
+            String regexToTest = req.getParameter("filterRegex");
+            try {
+                Pattern.compile(regexToTest);
+                this.filterRegex = regexToTest;
+            } catch (PatternSyntaxException x) {
+                Logger.getLogger(ListView.class.getName()).log(Level.WARNING, "Regex filter expression is invalid", x);
+            }
+        } else {
+            this.filterRegex = null;
+        }
+        this.statusButtonSize = json.getString("statusButtonSize");
+        this.layoutHeightRatio = json.getString("layoutHeightRatio");
+        /************************/
+
+        save();
+    }
+
+    @Override
+    public Item doCreateItem(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
+        return Jenkins.getInstance().doCreateItem(req, rsp);
+    }
+
+    @Override
+    public boolean contains(TopLevelItem item) {
+        return false;
+    }
+
+
+    /********************/
+    //Bellow all from mission control:
+    /********************/
+    protected Object readResolve() {
+        if (getBuildsLimit == 0)
+            getBuildsLimit = 250;
+
+        if (fontSize == 0)
+            fontSize = 16;
+
+        if (buildHistorySize == 0)
+            buildHistorySize = 16;
+
+        if (buildQueueSize == 0)
+            buildQueueSize = 10;
+
+        if (statusButtonSize == null)
+            statusButtonSize = "";
+
+        if (layoutHeightRatio == null)
+            layoutHeightRatio = "6040";
+
+        return this;
+    }
+    public int getFontSize() {
+        return fontSize;
+    }
+
+    public int getBuildHistorySize() {
+        return buildHistorySize;
+    }
+
+    public int getBuildQueueSize() {
+        return buildQueueSize;
+    }
+
+    public boolean isUseCondensedTables() {
+        return useCondensedTables;
+    }
+
+    public String getTableStyle() {
+        return useCondensedTables ? "table-condensed" : "";
+    }
+
+    public String getStatusButtonSize() {
+        return statusButtonSize;
+    }
+
+    public String getLayoutHeightRatio() {
+        return layoutHeightRatio;
+    }
+
+    public String getFilterRegex() {
+        return filterRegex;
+    }
+
+    public String getTopHalfHeight() {
+        return layoutHeightRatio.substring(0, 2);
+    }
+
+    public String getBottomHalfHeight() {
+        return layoutHeightRatio.substring(2, 4);
+    }
+
+    @Override
+    public boolean hasPermission(final Permission p) { return true; }
+
+    /**
+     * This descriptor class is required to configure the View Page
+     */
+    @Extension
+    public static final class DescriptorImpl extends ViewDescriptor {
+        @Override
+        public String getDisplayName() {
+            return Messages.SynopsysDasbhoardView_DisplayName();
+        }
+    }
+
+    public Api getApi() {
+        return new Api(this);
+    }
+
+    @Exported(name="builds")
+    public Collection<Build> getBuildHistory() {
+        List<Job> jobs = Jenkins.getInstance().getAllItems(Job.class);
+        RunList builds = new RunList(jobs).limit(getBuildsLimit);
+        ArrayList<Build> l = new ArrayList<Build>();
+        Pattern r = filterRegex != null ? Pattern.compile(filterRegex) : null;
+
+        for (Object b : builds) {
+            Run build = (Run)b;
+            Job job = build.getParent();
+
+            // Skip Maven modules. They are part of parent Maven project
+            if (job.getClass().getName().equals("hudson.maven.MavenModule"))
+                continue;
+
+            // If filtering is enabled, skip jobs not matching the filter
+            if (r != null && !r.matcher(job.getName()).find())
+                continue;
+
+            Result result = build.getResult();
+            l.add(new Build(job.getName(),
+                    build.getFullDisplayName(),
+                    build.getNumber(),
+                    build.getStartTimeInMillis(),
+                    build.getDuration(),
+                    build.getUrl(),
+                    result == null ? "BUILDING" : result.toString()));
+        }
+
+        return l;
+    }
+
+    //added jobUrl
+    @ExportedBean(defaultVisibility = 999)
+    public class Build {
+        @Exported
+        public String jobName;
+        @Exported
+        public String buildName;
+        @Exported
+        public int number;
+        @Exported
+        public long startTime;
+        @Exported
+        public long duration;
+        @Exported
+        public String result;
+        @Exported
+        public String jobUrl;
+
+        public Build(String jobName, String buildName, int number, long startTime, long duration, String jobUrl, String result) {
+            this.jobName = jobName;
+            this.buildName = buildName;
+            this.number = number;
+            this.startTime = startTime;
+            this.duration = duration;
+
+            this.jobUrl = jobUrl;
+
+            this.result = result;
+
+
+        }
+    }
+
+    /*******/
+    //Test Function to get all info
+    // search by jobId ? or jobname
+    // add argument
+    @Exported(name="allJobData")
+    public Collection<JobData> getAllJobData(){
+
+
+       ArrayList<JobData> jobData = new  ArrayList<JobData>();
+        jobData.add(new JobData("ALL PROPERTIES OF THIS JOB RETURNED"));
+
+
+        return jobData;
+    }
+
+    //what's default visibility?
+    @ExportedBean(defaultVisibility = 999)
+    public class JobData{
+        @Exported
+        public String allProperties;
+
+        public JobData(String allProperties){
+            this.allProperties = allProperties;
+        }
+    }
+
+    /*******/
+
+    @Exported(name="allJobsStatuses")
+    public Collection<JobStatus> getAllJobsStatuses() {
+        List<Job> jobs = Jenkins.getInstance().getAllItems(Job.class);
+        ArrayList<JobStatus> statuses = new ArrayList<JobStatus>();
+        String status, name, url;
+        Pattern r = filterRegex != null ? Pattern.compile(filterRegex) : null;
+
+        for (Job j : jobs) {
+            // Skip matrix configuration sub-jobs and Maven modules
+            if (j.getClass().getName().equals("hudson.matrix.MatrixConfiguration")
+                    || j.getClass().getName().equals("hudson.maven.MavenModule"))
+                continue;
+
+            // If filtering is enabled, skip jobs not matching the filter
+            if (r != null && !r.matcher(j.getName()).find())
+                continue;
+
+            // Get the url to link it in the dashboard
+            url = j.getUrl();
+
+            if (j.isBuilding()) {
+                status = "BUILDING";
+            } else {
+                Run lb = j.getLastBuild();
+                if (lb == null) {
+                    status = "NOTBUILT";
+                } else {
+                    status = lb.getResult().toString();
+                }
+            }
+
+            ItemGroup parent = j.getParent();
+            if (parent != null && parent.getClass().getName().equals("com.cloudbees.hudson.plugins.folder.Folder")) {
+                name = parent.getFullName() + " / " + j.getName();
+            } else {
+                name = j.getName();
+            }
+
+            statuses.add(new JobStatus(name, status, url));
+        }
+
+        return statuses;
+    }
+
+    @ExportedBean(defaultVisibility = 999)
+    public class JobStatus {
+        @Exported
+        public String jobName;
+        @Exported
+        public String status;
+        @Exported
+        public String jobUrl;
+
+        public JobStatus(String jobName, String status, String jobUrl) {
+            this.jobName = jobName;
+            this.status = status;
+            this.jobUrl = jobUrl;
+        }
+    }
+
+    /*************************/
+}
---
+++ src/main/webapp/js/synopsys-dashboard-view.js
@@ -0,0 +1,166 @@
+function format_date(dt) {
+  return dt.getFullYear()
+    + '-' + (dt.getMonth()<9?'0':'') + (dt.getMonth() + 1)
+    + '-' + (dt.getDate()<10?'0':'') + dt.getDate()
+    + ' ' + (dt.getHours()<10?'0':'') + dt.getHours()
+    + ':' + (dt.getMinutes()<10?'0':'') + dt.getMinutes()
+    + ':' + (dt.getSeconds()<10?'0':'') + dt.getSeconds();
+}
+
+function format_interval(iv) {
+  if (iv < 1000) { return iv + 'ms'; }
+
+  ivStr = '';
+  // Days
+  if (iv > 86400000) {
+    ivStr = Math.floor(iv/86400000) + 'd';
+    iv = iv - Math.floor(iv/86400000)*86400000;
+  }
+  // Hours
+  if (iv > 3600000) {
+    ivStr += ' ' + Math.floor(iv/3600000) + 'h';
+    iv = iv - Math.floor(iv/3600000)*3600000;
+  }
+  // Minutes
+  if (iv > 60000) {
+    ivStr += ' ' + Math.floor(iv/60000) + 'm';
+    iv = iv - Math.floor(iv/60000)*60000;
+  }
+  // Seconds
+  if (iv > 1000)
+    ivStr += ' ' + Math.floor(iv/1000) + 's';
+  return ivStr;
+}
+
+function reload_jenkins_build_queue(tableSelector, jenkinsUrl, buildQueueSize) {
+  $.getJSON( jenkinsUrl + '/queue/api/json', function( data ) {
+    // Remove all existing rows
+    $(tableSelector + ' tbody').find('tr').remove(); 
+    i = 0;
+    $.each( data.items, function( key, val ) {
+      i++;
+      if (i > buildQueueSize) {
+        return;
+      }
+      startDate = new Date(val.inQueueSince);
+      now = new Date();
+      waitingFor = now.getTime() - val.inQueueSince;
+      taskName = val.task.name.replace(/(,?)\w*=/g, "$1");
+      newRow = '<tr><td class="text-left"><a href="' + val.task.url + '">'+ taskName + '</a></td><td>' + format_date(startDate) + '</td><td>' + format_interval(waitingFor) + '</td></tr>';
+      $(tableSelector + ' tbody').append(newRow);
+    });
+  });
+}
+
+function reload_jenkins_node_statuses(divSelector, jenkinsUrl, nodeStatuses, buttonClass) {
+  $.getJSON( jenkinsUrl + '/computer/api/json', function( data ) {
+    // Remove all existing rows
+    $(divSelector + ' button').remove();
+    $.each( data.computer, function( key, val ) {
+      classes = !val.offline ? 'btn-success' : 'btn-danger';
+      if (val.displayName == "master")
+        nodeLinkName = '(master)';
+      else
+        nodeLinkName = val.displayName;
+      newDiv = '<a href="' + jenkinsUrl + '/computer/' + encodeURIComponent(nodeLinkName) + '/"><button class="btn ' + buttonClass + ' ' + classes + ' col-lg-6">' + val.displayName + ' &#47; ' + val.numExecutors + '</button></a>';
+      $(divSelector).append(newDiv);
+    });
+  });
+}
+
+function reload_jenkins_build_history(tableSelector, viewUrl, buildHistorySize) {
+  $.getJSON( viewUrl + '/api/json', function( data ) {
+    // Remove all existing rows
+    $(tableSelector + ' tbody').find('tr').remove();
+    i = 0;
+    $.each( data.builds, function( key, val ) {
+      i++;
+      if (i > buildHistorySize) {
+        return;
+      }
+      dt = new Date(val.startTime + val.duration);
+      jobName = val.buildName.replace(/(.*) #.*/, '$1');
+      switch (val.result) {
+        case 'SUCCESS':
+          classes = '';
+          break;
+        case 'FAILURE':
+          classes = 'danger';
+          break;
+        case 'ABORTED':
+        case 'UNSTABLE':
+          classes = 'warning';
+          break;
+        case 'BUILDING':
+          classes = 'info invert-text-color';
+          break;
+        default:
+          console.log('Job: ' + val.jobName + ' Result: ' + val.result);
+          classes = '';
+      }
+      newRow = '<tr class="' + classes + '"><td class="text-left">' + jobName + '</td><td>' + val.number + '</td><td>' + format_date(dt) + '</td><td>' + format_interval(val.duration) + '</td></tr>';
+      $(tableSelector + ' tbody').append(newRow);
+    });
+  });
+}
+
+function reload_jenkins_job_statuses(divSelector, viewUrl, buttonClass) {
+  $.getJSON( viewUrl + '/api/json', function( data ) {
+    // Remove all existing divs
+    $(divSelector + ' button').remove();
+    $.each( data.allJobsStatuses, function( key, val ) {
+      switch (val.status) {
+        case 'SUCCESS':
+          classes = 'btn-success';
+          break;
+        case 'FAILURE':
+          classes = 'btn-danger';
+          break;
+        case 'ABORTED':
+        case 'UNSTABLE':
+          classes = 'btn-warning';
+          break;
+        case 'NOTBUILT':
+          classes = 'invert-text-color';
+          break;
+        case 'BUILDING':
+          classes = 'btn-info invert-text-color';
+          break;
+        default:
+          console.log('Job: ' + val.jobName + ' Status: ' + val.status);
+          classes = 'btn-primary';
+      }
+      console.log(val);
+      jobData = expand_jenkins_job_data(viewUrl, val.jobUrl);
+      newDiv =
+      '<button class="btn ' + buttonClass + ' ' + classes + ' col-lg-6">' + val.jobName +
+      '<p><a href="' + val.jobUrl + '">' + 'gtp->' + '</a></p>' +
+      '<p>' + 'expnd->' + jobData + '</p>' +
+      '</button>' +
+      '</a>';
+      $(divSelector).append(newDiv);
+    });
+  });
+}
+
+//pass arguments to get the specific info
+function expand_jenkins_job_data(viewUrl, jobUrl){
+    console.log('joburl: ' + jobUrl);
+    console.log('viewurl:' + viewUrl);
+    count = 0;
+    allProperties = "";
+    $.getJSON(viewUrl + '/api/json', function(data){
+        $.each(data.allJobData, function(key, val){
+        console.log(count++);
+        console.log('val.allProperties b4:' + allProperties);
+
+            allProperties = val.allProperties;
+            console.log("all properties after1:" + allProperties);
+            return false;
+        });
+        console.log("all properties after2:" + allProperties);
+        return false;
+    });
+    console.log("all properties after3:" + allProperties);
+    return allProperties;
+}
